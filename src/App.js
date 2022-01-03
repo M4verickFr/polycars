e17b046787f5c7801b672016d13f00f3499b758a
Maverick Perrollaz <maverick.perrollaz@pm.me>
2022-01-03 11:08:44 +0100

    configure axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import axios from "axios";
 import React, {useState} from 'react';
 import Header from './components/header';
 import Cars from './components/cars';
 import List from './components/list';
+
+const baseURL = "https://polytech-info734.herokuapp.com/api/v1/";
 
 const initialFormData = Object.freeze({
   title: "",
@@ -15,6 +18,15 @@ function App() {
   const [formData, updateFormData] = useState(initialFormData);
 
   const [cars, addCars] = useState([]);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      updateFormData(response.data);
+    });
+  }, []);
+
+  if (!formData) return null;
+
 
   return (
     <div className="App">
